a866b39ccb5e6af2974f16d1044c5c479460d302
Jenil Jani <jeniljani14@gmail.com>
2023-09-02 14:04:20 +0530

    web app updated

--- laptop_app.py
+++ laptop_app.py
@@ -2,54 +2,94 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
+from streamlit_option_menu import option_menu
 
-st.title("Laptop Price Prediction :computer:")
 
-pickle_in = open('pipe.pkl', 'rb')
-pipe = pickle.load(pickle_in)
-df_pickle_in = open('laptop.pkl', 'rb')
-laptop = pickle.load(df_pickle_in)
+st.markdown(
 
-name = st.selectbox("Laptop Name",laptop['name'].unique())
+    """<style>
+            footer {
+            visibility: hidden;
+            }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-pros_name = st.selectbox("Processor Name",laptop['pros_name'].unique())
+# Navigation
+with st.sidebar:
+    selected = option_menu(
+        menu_title='Menu',
+        menu_icon='menu-button-wide',
+        default_index=0,
+        options=['Description', 'Prediction'],
+        icons=['book', 'laptop'],
+        orientation='vertical'
+    )
 
-pros_gen = st.selectbox("Processor Genereation",laptop['pros_gen'].unique())
+if selected == 'Descripiton':
+    pass
 
-ram = st.selectbox("RAM",laptop['ram'].unique())
 
-ssd = st.selectbox("SSD",laptop['ssd'].unique())
 
-hdd = st.selectbox("HDD",laptop['hdd'].unique())
+def predict_func():
+    if selected == 'Prediction':
+        st.title("Laptop Price Prediction :computer:")
 
-graphic_card = st.selectbox("Graphic Card",laptop['graphic_card'].unique())
+        pickle_in = open('pipe.pkl', 'rb')
+        pipe = pickle.load(pickle_in)
+        df_pickle_in = open('laptop.pkl', 'rb')
+        laptop = pickle.load(df_pickle_in)
 
-os = st.selectbox("OS",laptop['os'].unique())
+        name = st.selectbox("Laptop Name", laptop['name'].unique())
 
-display = st.number_input("Display Size", min_value=11.6,max_value=17.3)
+        pros_name = st.selectbox("Processor Name", laptop['pros_name'].unique())
 
-touch = st.selectbox("Touchscreen",['Yes','No'])
+        pros_gen = st.selectbox("Processor Genereation",
+                                laptop['pros_gen'].unique())
 
-if touch == 'Yes':
-    touch = 1
-else:
-    touch = 0
+        ram = st.selectbox("RAM", laptop['ram'].unique())
 
-warranty = st.selectbox("Product Warranty",['1 Year','2 Year','3 Year'])
+        ssd = st.selectbox("SSD", laptop['ssd'].unique())
 
-if warranty == '1 Year':
-    warranty = 1
-elif warranty == '2 Year':
-    warranty = 2
-else:
-    warranty = 3
+        hdd = st.selectbox("HDD", laptop['hdd'].unique())
 
-rating = st.number_input("Product Rating",min_value=1.0,max_value=5.0)
+        graphic_card = st.selectbox(
+            "Graphic Card", laptop['graphic_card'].unique())
 
-if st.button("Predict Price"):
-    final_pred = [[name,pros_name,pros_gen,ram,ssd,hdd,graphic_card,os,display,touch,
-                   warranty,rating]]
-    
-    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(final_pred)[0]))))
-    
+        os = st.selectbox("OS", laptop['os'].unique())
 
+        display = st.number_input(
+            "Display Size", min_value=11.6, max_value=17.3)
+
+        touch = st.selectbox("Touchscreen", ['Yes', 'No'])
+
+        if touch == 'Yes':
+            touch = 1
+        else:
+            touch = 0
+
+        warranty = st.selectbox("Product Warranty", [
+            '1 Year', '2 Year', '3 Year'])
+
+        if warranty == '1 Year':
+            warranty = 1
+        elif warranty == '2 Year':
+            warranty = 2
+        else:
+            warranty = 3
+
+        rating = st.number_input(
+            "Product Rating", min_value=1.0, max_value=5.0)
+
+        if st.button("Predict Price"):
+
+            final_pred = [[name, pros_name, pros_gen, ram, ssd, hdd, graphic_card, os, display, touch,
+                        warranty, rating]]
+
+            st.title("The predicted price of lapotop is: " +
+                    str(int(np.exp(pipe.predict(final_pred)[0]))))
+
+
+if __name__ == '__main__':
+    predict_func()
